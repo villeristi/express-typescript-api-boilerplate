fa6e8f78467d26e3abe6e3f8284b03ac1fc61b05
Ville Ristimäki <villeristimaki@gmail.com>
2018-03-16 01:16:20 +0200

    Simplify RouteResolver

--- src/common/classes/RouteResolver.ts
+++ src/common/classes/RouteResolver.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
-import { BaseRouteInterface } from './BaseRoute';
 import debug from '../util/debug';
 
 export default class RouteResolver {
-  private routes: BaseRouteInterface[] = [];
   private router;
 
-  public add(route: any) {
-    this.routes.push(new route());
+  /**
+   * RouteResolver constructor
+   * @param {e.Router} router
+   */
+  constructor(router: Router) {
+    this.router = router;
   }
 
   /**
    * Attach routes to router
-   * @param {e.Router} router
+   * @param route
    */
-  public addToRouter(router: Router): void {
-    this.router = router;
-    this.routes.forEach((routeClass: any) => {
-      debug('Route-class: ', routeClass);
-      routeClass.attachToRouter(this.router);
-    });
+  public add(route: any): void {
+    const routeClass = new route();
+    debug('Route-class: ', routeClass);
+    routeClass.attachToRouter(this.router);
   }
 }

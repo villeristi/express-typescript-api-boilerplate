d303d8c3d6c0a6479c1b4d7941f12aeb5972d832
Ville Ristimäki <villeristimaki@gmail.com>
2018-03-29 10:57:20 +0300

    Introduce middleware-prop to BaseRoute

--- src/common/classes/BaseRoute.ts
+++ src/common/classes/BaseRoute.ts
@@ -5,9 +5,10 @@ export default abstract class BaseRoute implements BaseRouteInterface {
 
   abstract method;
   abstract endpoint;
+  public middleware = new Array();
 
   public attachToRouter(router: Router): void {
-    router[this.method.toLowerCase()](this.endpoint, this.respond);
+    router[this.method.toLowerCase()](this.endpoint, [...this.middleware], this.respond);
   }
 
   abstract respond(req: Request, res: Response, next: NextFunction): any;

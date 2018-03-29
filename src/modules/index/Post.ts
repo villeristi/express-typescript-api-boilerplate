f3d56073241d943c91f235d67c8ed7d26b879496
Ville Ristimäki <villeristimaki@gmail.com>
2018-03-29 10:59:58 +0300

    Move index-modules to own folder

---
+++ src/modules/index/Post.ts
@@ -0,0 +1,13 @@
+import { Response } from 'express';
+
+import BaseRoute from '../../common/classes/BaseRoute';
+
+export default class Post extends BaseRoute {
+
+  method = 'POST';
+  endpoint = '/';
+
+  respond(req, res, next): Response {
+    return res.json({ msg: 'Hallo from post!'});
+  }
+}
